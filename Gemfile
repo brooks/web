fbdfd11ea66cc5a16905b7dc257f3e02f4fd9da4
Broppler <brooksmason@gmail.com>
2013-03-26 01:27:13 -0700

    header finished

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source :rubygems
+
+# PostgreSQL driver
+gem 'pg'
+
+# Sinatra driver
+gem 'sinatra'
+gem 'sinatra-contrib'
+
+# Use Thin for our web server
+gem 'thin'
+
+gem 'activesupport'
+gem 'activerecord'
+
+gem 'rake'
+gem "sinatra-twitter-bootstrap", "~> 2.2.2.1"
+
+group :test do
+  gem 'faker'
+  gem 'rspec'
+end
---
+++ public/javascript/application.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $('#contact').click(function(){
+    $('.icons').toggle();
+    $('.icon-envelope').addClass('animated bounceInDown');
+    $('.icon-github').addClass('animated bounceInDown');
+    $('.icon-linkedin').addClass('animated bounceInDown');
+    $('.icon-twitter').addClass('animated bounceInDown');
+  });
+
+});
+
