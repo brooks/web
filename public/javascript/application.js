d5aa42cc18447042bd7ff98a28bdce639dc9eac3
Broppler <brooksmason@gmail.com>
2013-04-01 21:34:28 -0700

    bat signal

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
-  $('#contact').click(function(){
+  $('#bat').click(function(){
     $('.icons').toggle();
     $('.icon-envelope').addClass('animated bounceInDown');
+    $('.icon-home').addClass('animated bounceInDown');
     $('.icon-github').addClass('animated bounceInDown');
     $('.icon-linkedin').addClass('animated bounceInDown');
     $('.icon-twitter').addClass('animated bounceInDown');
   });
-
 });
 
