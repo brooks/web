dff31aeb744e0a670365bb345ed280a37007de90
Broppler <brooksmason@gmail.com>
2013-04-01 22:31:41 -0700

    responsive design icons

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 
   $('#bat').click(function(){
     $('.icons').toggle();
-    $('.icon-envelope').addClass('animated bounceInDown');
-    $('.icon-home').addClass('animated bounceInDown');
-    $('.icon-github').addClass('animated bounceInDown');
-    $('.icon-linkedin').addClass('animated bounceInDown');
-    $('.icon-twitter').addClass('animated bounceInDown');
+    $('.awesome').addClass('animated bounceInDown');
+    $('.awesome').addClass('animated bounceInDown');
+    $('.awesome').addClass('animated bounceInDown');
+    $('.awesome').addClass('animated bounceInDown');
+    $('.awesome').addClass('animated bounceInDown');
   });
 });
 
