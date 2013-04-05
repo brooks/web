48414801865154055f0a61549e3e0426caa251f7
Broppler <brooksmason@gmail.com>
2013-04-04 17:23:40 -0700

    toggled resume and new content

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
 
+  $('.pdf').hover(
+    function() {
+      $('.text-box').show();
+    },
+    function() {
+      $('.text-box').hide();
+    }
+  );
+
+  $('.pdf').click(function(){
+    window.open('images/bmason_resume.pdf');
+  });
+
   $('#bat').click(function(){
+    $('.pdf-bar').toggle();
+    $('.pdf').addClass('animated fadeIn');
     $('.icons').toggle();
     $("html, body").animate({ scrollTop: 0 }, "slow");
     $('.awesome').addClass('animated fadeInDown');
