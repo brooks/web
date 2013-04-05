fb938c03952f0b8f71cf599a7c218801dbd50d94
Broppler <brooksmason@gmail.com>
2013-04-04 18:53:31 -0700

    changed bat switch behavior

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
-
+  var whiteBat = '/images/batman-signal-image.png' 
+  var blackBat = '/images/batman-signal-image-hover.png'
+  
   $('.pdf').hover(
     function() {
       $('.text-box').show();
+      $('.text-box').addClass('animated fadeIn');
     },
     function() {
       $('.text-box').hide();
+    }
+  );
+
+  $('#bat').hover(
+    function() {
+      $(this).attr('src', blackBat);
+    },
+    function() {
+      $(this).attr('src', whiteBat);
     }
   );
 
@@ -21,4 +33,3 @@ $(document).ready(function(){
     $('.awesome').addClass('animated fadeInDown');
   });
 });
-
