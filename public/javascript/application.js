c8616ce91cb5a9fbe2a282186d4681c93be89b12
brooks <brooksmason@gmail.com>
2014-02-08 23:45:07 -0800

    simplify design, remove content

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,35 +1,24 @@
-$(document).ready(function(){
-  var whiteBat = '/images/batman-signal-image.png' 
-  var blackBat = '/images/batman-signal-image-hover.png'
-  
-  $('.pdf').hover(
-    function() {
-      $('.text-box').show();
-      $('.text-box').addClass('animated fadeIn');
-    },
-    function() {
-      $('.text-box').hide();
-    }
-  );
-
-  $('#bat').hover(
-    function() {
-      $(this).attr('src', blackBat);
-    },
-    function() {
-      $(this).attr('src', whiteBat);
-    }
-  );
-
-  $('.pdf').click(function(){
+$(document).ready(function(){  
+  $('#bat-signal-div').click(function(){
     window.open('images/bmason_resume.pdf');
   });
 
-  $('#bat').click(function(){
-    $('.pdf-bar').toggle();
-    $('.pdf').addClass('animated fadeIn');
-    $('.icons').toggle();
-    $("html, body").animate({ scrollTop: 0 }, "slow");
-    $('.awesome').addClass('animated fadeIn');
-  });
+  $('.icon-cloud-download').hover(
+    function() {
+      $('.resume').show()
+      $('.resume').addClass('animated fadeIn');
+
+    },
+    function() {
+      $('.resume').hide()
+    }
+  );
+
+
+    $('.icons').show();
+    $('.awesome').addClass('animated bounceInLeft');
+    $('#bat-signal-div').addClass('animated bounceInLeft');
+    $('.nameplate-text').addClass('animated bounceInUp');
+
+
 });
